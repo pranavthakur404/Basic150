b48f22aa72e38946977442cbb6c9c2f4e6ab8cfe
pranavthakur404 <pranavufc@gmail.com>
2024-01-05 19:41:18 +0530

    Update question51.js

--- Basic150/js/question51.js
+++ Basic150/js/question51.js
@@ -1,6 +1,7 @@
 // Write a JavaScript program to convert a given number into hours and minutes.
 function convert_into_hour(num) {
   let hour = Math.floor(num / 60);
+  let min = num % 60;
   return hour + ":" + min;
 }
 console.log(convert_into_hour(71));
