ee13b69b131f24c5678197561d9eab4d95f514d7
pranavthakur404 <pranavufc@gmail.com>
2024-01-07 00:00:47 +0530

    Update question64.js

--- Basic150/js/question64.js
+++ Basic150/js/question64.js
@@ -1,17 +1,19 @@
 // Write a JavaScript program to concatenate two strings and return the result. If the length of the strings does not match, then remove the characters from the longer string.
 function concateString(str1, str2) {
-  if (str1.length == str1.length) {
+  if (str1.length == str2.length) {
     return str1 + " " + str2;
   } else {
     return str1.length > str2.length
       ? str1
           .split("")
           .slice(str1.length - str2.length, str1.length)
-          .join("")
-      : str2
-          .split("")
-          .slice(str2.length - str1.length, str2.length)
-          .join("");
+          .join("") + str2
+      : str1 +
+          str2
+            .split("")
+            .slice(str2.length - str1.length, str2.length)
+            .join("");
   }
 }
 console.log(concateString("Python", "JS"));
+console.log(concateString("ab", "cdef"));
