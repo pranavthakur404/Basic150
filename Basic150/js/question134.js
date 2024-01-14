ae22c4baa4bfcf00305f830c40f57ba94be9b058
pranavthakur404 <pranavufc@gmail.com>
2024-01-14 18:05:06 +0530

    Update question 134 and 142
    Update question 134 and 142

--- Basic150/js/question134.js
+++ Basic150/js/question134.js
@@ -1,4 +1,11 @@
-// Write a JavaScript program to change the characters (lower case) in a string where a turns into z, b turns into y, c turns into x, ..., n turns into m, m turns into n, ..., z turns into a. 
-function changeChar(){arr}{
-    
+// Write a JavaScript program to change the characters (lower case) in a string where a turns into z, b turns into y, c turns into x, ..., n turns into m, m turns into n, ..., z turns into a.
+function changeChar(str) {
+  let newString = [];
+  for (let i = 0; i < str.length; i++) {
+    let charOrder = str.charCodeAt(i) - "a".charCodeAt(0);
+    let changeChar = 25 - charOrder + "a".charCodeAt(0);
+    newString.push(String.fromCharCode(changeChar));
+  }
+  return newString.join("");
 }
+console.log(changeChar("pranav"));
--- Basic150/js/question142.js
+++ Basic150/js/question142.js
@@ -1,1 +1,10 @@
-// Write a JavaScript program to simplify a given absolute path for a file in Unix-style.d  
+// Write a JavaScript program to simplify a given absolute path for a file in Unix-style.d
+function simplifiedPath(mainPath) {
+  mainPath = mainPath.split("/");
+  mainPath = mainPath.filter((char) => {
+    return char !== "." && char !== ".." && char !== "";
+  });
+
+  return mainPath.join("/");
+}
+console.log(simplifiedPath("/home/var/./www/../html//sql/"));
